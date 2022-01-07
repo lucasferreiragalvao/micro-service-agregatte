ba3b21eefc35fa4bb0c878a88e46b9cfbf915ceb
lucasferreiragalvao <35471086+lucasferreiragalvao@users.noreply.github.com>
2022-01-06 23:49:08 -0300

    feat: add validation status in progress when finished reserve (#12)

--- src/main/java/com/unifacef/work/groupone/microservicereserve/exceptions/MessageKey.java
+++ src/main/java/com/unifacef/work/groupone/microservicereserve/exceptions/MessageKey.java
@@ -11,6 +11,7 @@ public enum MessageKey {
     RESERVE_FINAL_ODOMETER_LESS_THAN_START_ODOMETER("reserve.final.odometer.less.than.start.odometer"),
     RESERVE_IS_ALREADY_IN_PROGRESS("reserve.is.already.in.progress"),
     RESERVE_IS_ALREADY_FINISHED("reserve.is.already.finished"),
+    RESERVE_NOT_FINISHED_BECAUSE_STATUS_IN_PROGRESS("reserve.not.finished.because.status.in.progress"),
     EMPLOYEE_NOT_CAN_EXECUTE_RENTAL("employee.not.can.execute.rental");
 
     private String key;
